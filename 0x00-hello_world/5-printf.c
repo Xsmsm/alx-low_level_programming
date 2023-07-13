510fc28c3d36dca788de57d3c80ee7f7372e6cc6
xsmsm <smesraa@yahoo.ca>
2023-07-13 08:33:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this is to use the printf function
+ *Return: o correct
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
