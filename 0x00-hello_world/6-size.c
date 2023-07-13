2fa7662d3686ef4543c44af50fe707f4d47f6d1f
xsmsm <smesraa@yahoo.ca>
2023-07-13 09:34:34 -0700

    printing the data size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - this is the program for printing the siza of data
+ * Return: 0 correct
+ */
+int main(void)
+{      
+	char ch;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(ch));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
+}
+
