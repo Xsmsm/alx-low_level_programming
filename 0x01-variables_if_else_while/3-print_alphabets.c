c73038fc493f6d194b3c6906e9c2b9f355cbdbe0
xsmsm <smesraa@yahoo.ca>
2023-07-15 10:59:45 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main- main start
+ *Return: always 0
+ */
+int main(void)
+{
+	int n = 96;
+	int u = 65;
+
+	while (n <= 120);
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (u <= 90);
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
