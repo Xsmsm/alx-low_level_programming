8d91b13a2ad4b1e45ddcb6198b31ec3b1dd8f82c
xsmsm <smesraa@yahoo.ca>
2023-07-16 05:11:15 -0700

    ss

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n >= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
