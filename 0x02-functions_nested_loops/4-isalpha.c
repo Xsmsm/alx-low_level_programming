a6c9aeb02926167008996b611e53cacd676f341a
xsmsm <smesraa@yahoo.ca>
2023-07-19 10:09:11 -0700

    sd

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks if c is alphabet
- * @ c: the checked character
+ * @c: the checked character
  * Return: 1 if it's a charachter and 0 if not
  */
 
@@ -10,11 +10,11 @@ int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	Return (0);
+	return (0);
 }
