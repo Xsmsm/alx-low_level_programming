139926839e0ceab176f22d77a691edd2ba958142
xsmsm <smesraa@yahoo.ca>
2023-07-20 07:31:54 -0700

    sd

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,8 +9,8 @@ void positive_or_negative(int i)
 	}
 
 	else if (i == 0)
-		printf("%d is zero", i);
+		printf("%d is zero\n", i);
 
 	else
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
 }
