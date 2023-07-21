f7638e638f27120a8396913ee6f531b6413c2460
xsmsm <smesraa@yahoo.ca>
2023-07-21 07:24:22 -0700

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' || c != '4')
+		{	
+			_putchar(n);
+			n++;
+		}	
+		else
+			n++;
+	}
+}
