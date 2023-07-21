5f400b74bcb2cba97f8398dbcf9a847ed02c67fe
xsmsm <smesraa@yahoo.ca>
2023-07-21 07:05:22 -0700

    fg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints from 0 to 9
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n >= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
