d7390b954b9359e8e538151a03c3ee4646657b9c
xsmsm <smesraa@yahoo.ca>
2023-07-25 04:13:58 -0700

    f

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(s);
+		s = s + 2;
+	}
+	_putchar('\n');
+}
+
