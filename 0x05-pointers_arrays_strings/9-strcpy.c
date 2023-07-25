b7bf10a0277eb3e424627925b3fe18abf793430e
xsmsm <smesraa@yahoo.ca>
2023-07-25 10:07:55 -0700

    f

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char[] to;
-	char[] from;
-	char[] copied;
+	int i = 0;
 
-	from = *src;
-	to = *dest;
-
-	copied = from + to;
-	return(copied);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
 }
 
 
