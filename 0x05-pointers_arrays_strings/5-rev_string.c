f0664350f3fbc5548c801e1d13b7ba043ff2310d
xsmsm <smesraa@yahoo.ca>
2023-07-25 02:32:18 -0700

    d

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int N = 0;
+
+	while (*s != '\0')
+	{
+		N++;
+		s++;
+	}
+
+	s--;
+
+	while (N != 0)
+	{
+		_putchar(*s);
+		N--;
+		s--;
+	}
+}
+
