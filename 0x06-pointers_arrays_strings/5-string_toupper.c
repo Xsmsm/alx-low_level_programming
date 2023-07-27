86533053d71b18fbd488d6189177b7c810a17a71
xsmsm <smesraa@yahoo.ca>
2023-07-27 09:44:08 -0700

    k

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,10 +7,15 @@
  */
 char *string_toupper(char *n)
 {
-	int c;
+	int i;
 
-	c = 0;
-
-	while (n[c] != '\0')
+	i = 0;
+	while (n[i] != '\0')
 	{
-		j[c] = c + 32;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
+
