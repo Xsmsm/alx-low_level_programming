652a26d823fee6392d9f5468daf16742b3568241
xsmsm <smesraa@yahoo.ca>
2023-07-27 06:21:38 -0700

    g

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int countd = 0;
-	int counts = 0;
+	int i;
+	int j;
 
-	while (dest != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
-		countd++;
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	while (src != '\0')
-	{
-		dest[countd] = src[counts];
-		counts++;
-		countd++;
-	}
-
-	dest[countd] = '\0';
-
+	dest[i] = '\0';
 	return (dest);
 }
+
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int c;
+
+	c = 0;
+
+	while (n[c] != '\0')
+	{
+		j[c] = c + 32;
