a8e19ff3553f25f041a64096ef0ce41fe6b26c97
xsmsm <smesraa@yahoo.ca>
2023-08-03 07:27:26 -0700

    g

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
+	int longit = 0;
 
 	if (*s)
 	{
-		length++;
-		_strlen_recursion(s + 1);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	return(length);
+
+	return (longit);
 }
+
