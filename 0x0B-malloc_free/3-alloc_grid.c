fa5ebd1bdbb8b09d1ba28cea19fe41cc5ba40acf
xsmsm <smesraa@yahoo.ca>
2023-08-09 04:14:37 -0700

    f

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *newm;
+	int i = 0;
+	int n = 0;
+	int r = 0;
+	int d = 0;
+
+	if ((s1 == NULL) || (s2 == NULL)
+		return (NULL);
+
+	while (s1[i] != '\0')
+		i++;
+
+	while (s2[n] != '\0')
+		n++;
+
+	newm = malloc(sizeof(char) * (i + n + 1);
+
+	while (s1[r] != '\0')
+	{
+		newm[r] = s1[r];
+		r++;
+	}
+	while (s2)
+	{
+		newm[r] = s2[d];
+	}
+	newm[r] = '\0';
+	return (newm);
+}
